a59899da788702154f5d7a011e774090e7cb1fd5
kadetete <kadetesan@gmail.com>
2024-01-03 07:23:14 -0300

    initial commit

---
+++ src/main/java/br/edu/ifrn/provadois/ProvadoisApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.provadois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvadoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvadoisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifrn/provadois/ProvadoisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.provadois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvadoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
